9eb141223d02a66aed8aeb64acd3a31bc869403b
Mohd Kashif <mohd.kashif@sabpaisa.in>
2025-08-01 12:42:26 +0530

    Green: Handle negative number

--- string_calculator.py
+++ string_calculator.py
@@ -1,12 +1,26 @@
+from exceptions import NegativeNumberError
+
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
     
-    numbers = numbers.replace("\n", ",")
+    clean_numbers = numbers.replace("\n", ",")
     
-    parts = numbers.split(",")
-    total = 0
+    parts = clean_numbers.split(",")
+    
+    negatives = []
     for part in parts:
-        total += int(part)
+        number = int(part)
+        if number < 0:
+            negatives.append(number)
     
-    return total
+    if len(negatives) > 0:
+        negative_str = ", ".join(map(str, negatives))
+        error_msg = f"negative numbers not allowed: {negative_str}"
+        raise NegativeNumberError(error_msg)
+    
+    result = 0
+    for part in parts:
+        result += int(part)
+    
+    return result
