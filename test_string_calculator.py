b4341b65c3f20912539f9cbb27936298ec5a860e
Mohd Kashif <mohd.kashif@sabpaisa.in>
2025-08-01 12:25:46 +0530

    Red: add test for new line

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -4,10 +4,17 @@ from string_calculator import add
 def test_empty_string_returns_zero():
     assert add("") == 0
 
-
-
 def test_single_number_returns_itself():
     assert add("1") == 1
 
 def test_two_numbers_returns_sum():
-    assert add("1,5") == 6
+    assert add("1,7") == 8
+
+def test_multiple_numbers_returns_sum():
+    assert add("1,2,3,4,5") == 15
+
+def test_newlines_between_numbers():
+    assert add("1\n2,3") == 6
+    
+def test_mixed_delimiters():
+    assert add("1,2\n3,4") == 10
